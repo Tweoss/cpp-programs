78b503ea11ee0dd0d489f72c7807cd997a6c4959
calispi <fchua490@student.fuhsd.org>
2020-10-07 20:33:25 -0700

    finished number 7

--- submissions.cpp
+++ submissions.cpp
@@ -1,132 +1,85 @@
 #include <iostream>
-#include <array>
-#include <vector>
 #include <string>
-#include <sstream>
 
-//! ASSIGNMENT: Example Midterm
+//! ASSIGNMENT: #7 Functions and Arrays
 
+//* 
+//* 
+//* 
+//* 
+//* 
+//* 
+//* 
+//* 
+//* 
 
-//* Instead of returning an object, I will return an array with the number of each grade, then the number of invalids
-int* analyzeScores(int arr[], int n) {
-	int a=0,b=0,c=0,d=0,f=0,inval=0;
+void countPositiveNegative(int arr[], int n, int target[])
+{
 	for (size_t i = 0; i < n; i++)
 	{
-		if (arr[i] >= 90) {
-			a++;
+		if (arr[i] > 0) {
+			target[0]++;
 		}
-		else if (arr[i] >= 80) {
-			b++;
+		else if (arr[i]<0) {
+			target[1]++;
 		}
-		else if (arr[i] >= 70) {
-			c++;
+		else if (arr[i]==0) {
+			target[2]++;
 		}
-		else if (arr[i] >= 60) {
-			d++;
-		}
-		else if (arr[i] >= 0) {
-			f++;
-		}
-		else {
-			inval++;
-		}
-	}
-	int * result;
-	result = new int[6];
-	result[0] = a;
-	result[1] = b;
-	result[2] = c;
-	result[3] = d;
-	result[4] = f;
-	result[5] = inval;
-	return result;
-}
-
-void printRectangle(char out, int width, int height) {
-	int i,j;
-	for (i=0;i<height;i++) {
-		for (j=0;j<width;j++) {
-			if ((i==0 || i==height-1) || (j==0 || j==width-1)) {
-				std::cout << out;
-			}
-			else {
-				std::cout << " ";
-			}
-		}
-		std::cout << std::endl;
 	}
 }
 
-//* will modify input string instead of create new one
-bool extractComment(std::string & input) {
-	bool output = false;
-	if (input.length() > 3) {
-		if (input[0] == '/' && input[1] == '/' && input[2] == ' ') {
-			output = true;
-			input[0] = ' '; input[1] = ' '; 
-			std::rotate(input.begin(),input.begin() + 3, input.end());
+void rotateRight(int arr[], int n) {
+	int temp = arr[n-1];
+	for (size_t i = n-1; i > 0; i--)
+	{
+		arr[i] = arr[i-1];
+	}
+	arr[0] = temp;
+	
+}
+
+int countNonAlpha(std::string input) {
+	int counter = 0;
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		if (!(input[i]==32 || (input[i]>=65 && input[i]<=90)|| (input[i]>=97 && input[i]<=122))) {
+			counter++;
 		}
+	}
+	return counter;
+}
+
+int deleteZeros(int arr[], int n) {
+	int output = 0;
+	for (size_t i = 0; i < n; i++)
+	{
+		if (arr[i] == 0) {
+			while (arr[i+output] == 0) {
+				output++;
+			}
+		}
+		// std::cout << i << "," << arr[i] << "," << arr[i+output] << std::endl;
+		arr[i] = arr[i+output];
+	}
+	for (size_t i = n-output; i < n; i++)
+	{
+		arr[i] = 0;
 	}
 	return output;
 }
 
-bool checkAgainstVariableNamingRules(std::string input) {
-	bool output = true;
-	if (input.length() > 255 || input.length() == 0) {
-		output = false;
-	}
-	else if (input[0] != 95 && !(input[0] > 64 && input[0] < 91) && !(input[0] > 96 && input[0] < 123)){
-		output = false;
-	}
-	else {
-		for (size_t i = 0; i< input.length(); i++) {
-			if (!(input[i] == 95 || (input[i] > 47 && input[i] < 58) || (input[i] > 64 && input[i] < 91) || (input[i] > 96 && input[i] < 123))) {
-				output = false;
-				break;
-			}
+int reverseCase(std::string input) {
+	int output;
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		if (input[i] <= 90 && input[i] >=65) {
+			input[i]+=32;
+			output++;
 		}
-	}
-	// std::cout << input[0] << std::endl;
-	// std::cout << (int) input[1] << std::endl;
-	// std::cout << (int) input[2] << std::endl;
-	// std::cout << (int) input[3] << std::endl;
-	// std::cout << (int) input[4] << std::endl;
-	// std::cout << (int) input[5] << std::endl;
-	// std::cout << (int) input[6] << std::endl;
-	// std::cout << (int) input[7] << std::endl;
-	// std::cout << (int) input[8] << std::endl;
-	// std::cout << (int) input[9] << std::endl;
-	return output;
-}
-
-bool isBoundedList(void) {
-	int max, min;
-	int input, first_input, last_input;
-	bool output = false;
-	std::cout << "Please enter non-negative score: ";
-	std::cin >> first_input;
-	max = first_input; min = first_input;
-	input = first_input;
-	last_input = first_input;
-	while (input >= 0) {
-		last_input = input;
-		if (input > max) {
-			max = input;
-		}
-		else if (input < min) {
-			min = input;
-		}
-		std::cout << "Please enter non-negative score: ";
-		std::cin >> input;
-	}
-	if (first_input == max) {
-		if (last_input == min) {
-			output = true;
-		}
-	}
-	else if (first_input == min) {
-		if (last_input == max) {
-			output = true;
+		else if (input[i] <= 122 && input[i] >=97) {
+			input[i]-=32;
+			output++;
 		}
 	}
 	return output;
@@ -134,83 +87,46 @@ bool isBoundedList(void) {
 
 int main(void)
 {
-	std::cout << "TESTING analyzeScores\n";
-	std::cout << "Enter space-delimited array of integers for grades: ";
-	std::vector<int> input;
-	int size = 0;
-	std::string inputstr;
-	std::getline(std::cin, inputstr);
-	std::istringstream sstr(inputstr);
-	int temp;
-	while(sstr >> temp) {
-		input.push_back(temp);
-		size++;
-	}
-	
-	int arr[size];
-	for (size_t i = 0; i < size; i++)
+	int input[] = {-3,-2,-1,0,0,0,1,2,3,4};
+	size_t sizeinput = sizeof(input)/sizeof(int);
+	std::string string = "0123abcd: HeyOitsME.?";
+	int output[3] = {0};
+	int intout;
+
+	countPositiveNegative(input,sizeinput,output);
+	std::cout << "Testing countPositiveNegative\n";
+	for (size_t i = 0; i < 3; i++)
 	{
-		arr[i]=input[i];
+		std::cout << output[i] << " ";
 	}
-	
-	int * output;
-	output = new int[6];
-	output = analyzeScores(arr,size);
-	std::cout << "Output of analyzeScores:" << std::endl;
-	std::cout << "A:" << output[0] << ", " << "B:" << output[1] << ", " << "C:" << output[2] << ", " << "D:" << output[3] << ", " << "F:" << output[4] << ", " << "Fail:" << output[5];
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 
-	std::cout << std::endl << "TESTING printRectangle\n";
-	std::cout << "Enter a character: ";
-	char tempchar;
-	std::cin >> tempchar;
-	std::cout << "Enter the width: ";
-	int tempwidth;
-std::cin.clear();
-std::cin.ignore(1024,'\n');
-	std::cin >> tempwidth;
-	std::cout << "Enter the height: ";
-	int tempheight;
-std::cin.clear();
-std::cin.ignore(1024,'\n');
-	std::cin >> tempheight;
-	std::cout << "Output of printRectangle:" << std::endl;
-	printRectangle(tempchar,tempwidth,tempheight);
-	
-std::cin.clear();
-std::cin.ignore(1024,'\n');
+	std::cout << "Testing rotateRight\n";
+	rotateRight(input, sizeinput);
+	for (size_t i = 0; i < sizeinput; i++)
+	{
+		std::cout << input[i] << " ";
+	}
+	std::cout << std::endl << std::endl;
 
-	std::cout << std::endl << "TESTING extractComment\n";
-	std::cout << "Enter a string: ";
-	std::string inputstring;
-	std::getline(std::cin,inputstring);
-	bool tempbool;
-	tempbool = extractComment(inputstring);
-	std::cout << "\nOutput of extractComment:" << std::endl;
-	if (tempbool) {
-		std::cout << inputstring << std::endl;
-	}
-	else {
-		std::cout << "No comment to extract\n";
-	}
+	std::cout << "Testing countNonAlpha\n";
+	intout = countNonAlpha(string);
+	std::cout << intout;
+	std::cout << std::endl << std::endl;
 
-	std::cout << std::endl << "TESTING checkAgainstVariableNamingRules\n";
-	std::cout << "Enter a string: ";
-	std::getline(std::cin,inputstring);
-	tempbool = checkAgainstVariableNamingRules(inputstring);
-	std::cout << "\nOutput of checkAgainstVariableNamingRules:" << std::endl;
-	if (tempbool) {
-		std::cout << "Good Variable Name\n";
+	std::cout << "Testing deleteZeros\n";
+	intout = deleteZeros(input,sizeinput);
+	std::cout << "Changed " << intout << " zeros.\n";
+	for (size_t i = 0; i < sizeinput; i++)
+	{
+		std::cout << input[i] << " ";
 	}
-	else {
-		std::cout << "Bad Variable Name\n";
-	}
+	std::cout << std::endl << std::endl;
 
-	std::cout << std::endl << "TESTING isBoundedList\n";
-	std::cout << "Output of isBoundedList:" << std::endl;
-	tempbool = isBoundedList();
-	std::cout << std::boolalpha;   
-    std::cout<< tempbool <<"\n";
+	std::cout << "Testing reverseCase\n";
+	intout = reverseCase(string);
+	std::cout << "Changed " << intout << " letters" << std::endl;
+	std::cout << string << std::endl;
+	std::cout << std::endl << std::endl;
 	return 0;
 }
-
