e71e4a29584c66733c5f0e8811ba35fce955463b
tweoss <hwymonkey@gmail.com>
2020-11-10 21:23:58 -0800

    proj 2, before switching to only 1 int

--- submissions.cpp
+++ submissions.cpp
@@ -2,225 +2,158 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <fstream>
 
-//! ASSIGNMENT: selected questions in Chapter 13 
+// #include <tgmath.h>
 
-class Date
-{
-	//not using m_
+//! ASSIGNMENT: Manage Expenses Plus
+//? Assumes that the file has TWO SPACES between the dollar and description e.g. 12.34  Monthly day years expense
+//? (the pdf instructions uses two spaces. the example input files use a fixed length. using two spaces allows for very large numbers, while a fixed length does not. THIS program uses TWO spaces, like in the PDF INSTRUCTIONS)
+
+class Expense {
 	private:
-	int month, day, year; std::string month_string;
-
+	std::string adjusted_description;
+	int expense_int, expense_frac; 
+	std::string frac_string;
 	public:
-	Date() {
-		month = 12; day = 30; year = 2000; month_string = "December";
+	std::string original_description; 
+
+	Expense(int dollar, int cent, std::string descrip) {
+		expense_int = dollar; expense_frac = cent; 
+		original_description.assign(descrip);
+		std::transform(original_description.begin(), original_description.end(), adjusted_description.begin(), ::tolower);
+		frac_string.assign(std::to_string(expense_frac));
+		frac_string.insert(0, "0");
+		frac_string = frac_string.substr(frac_string.length()-2, frac_string.length()-0);
 	}
 
-	Date(int a, int b, int c) {
-		month = a;
-		day = b;
-		year = c;
-		if (day > 31 || day < 1 || month > 12 || month < 1) {
-			std::cout << "Error, date is not valid.\n";
-			exit(EXIT_FAILURE);
+	std::string print_expense() {
+		return std::to_string(expense_int).append(".").append(frac_string);
+	}
+
+	//* make sure to lowercase the search string first
+	bool search(std::string search_string)  {	
+		if (adjusted_description.find(search_string) != std::string::npos) {
+			return true;
 		}
-		switch (a)
-		{
-			case 1:
-				month_string.assign("January");
-				break;
-			case 2:
-				month_string.assign("February");
-				break;
-			case 3:
-				month_string.assign("March");
-				break;
-			case 4:
-				month_string.assign("April");
-				break;
-			case 5:
-				month_string.assign("May");
-				break;
-			case 6:
-				month_string.assign("June");
-				break;
-			case 7:
-				month_string.assign("July");
-				break;
-			case 8:
-				month_string.assign("August");
-				break;
-			case 9:
-				month_string.assign("September");
-				break;
-			case 10:
-				month_string.assign("October");
-				break;
-			case 11:
-				month_string.assign("November");
-				break;
-			
-			case 12:
-				month_string.assign("December");
-				break;
-		};
-	}
-	void print_slash() {
-		std::cout << month << "/" << day << "/" << year << std::endl;
-	}
-	void print_comma() {
-
-		std::cout << month_string << " " << day << ", " << year << std::endl;
-	}
-	void print_plain() {
-		std::cout << day << " " << month_string << " " << year << std::endl;
-	}
-};
-
-void test_date(void) {
-	int temp1, temp2, temp3, choice; 
-	choice = INT_MAX;
-	std::cout << "Testing Date class." << std::endl;
-	while (choice != 0) {
-		std::cout << "Enter month as a positive integer: ";
-		std::cin >> temp1;
-		std::cin.clear();
-		std::cin.ignore(INT_MAX,'\n');
-
-		std::cout << "Enter day as a positive integer: ";
-		std::cin >> temp2;
-		std::cin.clear();
-		std::cin.ignore(INT_MAX,'\n');
-
-		std::cout << "Enter year as a positive integer: ";
-		std::cin >> temp3;
-		std::cin.clear();
-		std::cin.ignore(INT_MAX,'\n');
-
-		Date date(temp1,temp2,temp3);
-
-		std::cout << "Enter 1 to format as 12/30/2000, 2 to format as December 30, 2000, 3 to format as 30 December 2000, or anything else to exit.\n";
-		std::cout << "Choice: ";
-		std::cin >> choice;
-		std::cin.clear();
-		std::cin.ignore(INT_MAX,'\n');
-		switch (choice)
-		{
-		case 1:
-			date.print_slash();
-			break;
-		case 2:
-			date.print_comma();
-			break;
-		case 3:
-			date.print_plain();
-			break;
-		default:
-			std::cout << "Exiting.\n";
-			choice = 0;
-			break;
+		else {
+			return false;
 		}
 	}
-}
-
-class Employee {
-	private:
-	std::string name, department, position; int idNumber;
-	public:
-	Employee() {
-		name.assign(""); department.assign(""); position.assign(""); idNumber = 0;
+	bool higher_than(int max) {
+		if (expense_int < max) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
-	Employee(std::string a, int b) {
-		name.assign(a); idNumber = b;
-		department.assign(""); position.assign("");
-	}
-	Employee(std::string a, int b, std::string c, std::string d) {
-		name.assign(a); idNumber = b; department.assign(c); position.assign(d);
-	}
-	void change_name(std::string a) {
-		name.assign(a);
-	}
-	void change_id(int a) {
-		idNumber = a;
-	}
-	void change_department(std::string a) {
-		department.assign(a);
-	}
-	void change_position(std::string a) {
-		position.assign(a);
-	}
-	std::string read_name() {
-		return name;
-	}
-	int read_id() {
-		return idNumber;
-	}
-	std::string read_department() {
-		return department;
-	}
-	std::string read_position() {
-		return position;
-	}
-
-
-};
-
-void test_employee(void) {
-	std::cout << "Testing Employee class." << std::endl;
-	Employee susan("Susan Meyers", 47899, "Accounting", "Vice President");
-	std::cout << susan.read_name() << ", a " << susan.read_position() << " from " << susan.read_department() << ", has id number " << susan.read_id() << ".\n";
-	Employee mark("Mark Jones", 39119, "IT", "Programmer");
-	std::cout << mark.read_name() << ", a " << mark.read_position() << " from " << mark.read_department() << ", has id number " << mark.read_id() << ".\n";
-	Employee joy("Joy Rogers", 81774, "Manufacturing", "Engineer");
-	std::cout << joy.read_name() << ", a " << joy.read_position() << " from " << joy.read_department() << ", has id number " << joy.read_id() << ".\n";
-}
-
-class Car {
-	private:
-	int yearModel, speed; std::string make;
-	public:
-	Car(int a, std::string b) {
-		yearModel = a;
-		make.assign(b);
-		speed = 0;
-	}
-	int get_year(void) {
-		return yearModel;
-	}
-	std::string get_make(void) {
-		return make;
-	}
-	int get_speed(void) {
-		return speed;
-	}
-	void accelerate(void) {
-		speed += 5;
-	}
-	void brake(void) {
-		speed -= 5;
+	void print() {
+		std::stringstream sstream;
+		int padding; std::string paddingstring;
+		if (expense_int == 0) {
+			paddingstring.assign("        ");
+		}
+		else {
+			padding = 8 - (int) floor(log(expense_int)/log(10));
+			// std::cout << log(420)/log(2.718281828);
+			paddingstring.assign("");
+			for (int i = 0; i < padding; i++)
+			{
+				paddingstring.append(" ");
+			}
+		}
+		sstream << "AMOUNT($" << print_expense() << ")" << paddingstring << "DESC(" << original_description << ")";
+		std::cout << sstream.str();
 	}
 };
 
-void test_car(void) {
-	std::cout << "Testing Car class." << std::endl;
-	Car car(2000, "XYZ CORP");
-	car.accelerate(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.accelerate(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.accelerate(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.accelerate(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.accelerate(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.brake(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.brake(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.brake(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.brake(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-	car.brake(); std::cout << "Speed is now " << car.get_speed() << ".\n";
-}
+class Container {
+	std::vector<Expense> list;
+	public:
 
-int main(void)
+	void load(std::string filename) {
+		std::string templine;
+		std::ifstream contentFile(filename);
+		if (contentFile.is_open()) {
+			while (std::getline(contentFile,templine)) {
+				//* splitting up the line to get the necessary fields
+				size_t index_of_period;
+				index_of_period = templine.find(".");
+				templine.find_first_not_of
+				list.push_back(Expense(std::stoi(templine.substr(0,index_of_period - 1)),std::stoi(templine.substr(index_of_period + 1,2)),templine.substr(index_of_period + 5,templine.length())));
+			}
+			contentFile.close();
+		}
+		else {
+			//* FAILURE
+			std::cout << "Failed to locate \"" << filename << ".\" Please check that the file exists.";
+			exit(0);
+		}
+	}
+
+	void show_all(void) {
+		for (size_t i = 0; i < list.size(); i++)
+		{
+			std::cout << list[i].print_expense() << std::endl;
+		}
+		
+	}
+	void search_string(std::string search) {
+		//* transform the string to lowercase (to ignore)
+		std::transform(search.begin(), search.end(), search.begin(), ::tolower);
+		for (size_t i = 0; i < list.size(); i++)
+		{
+			if (list[i].search(search)) {
+				std::cout << list[i].print_expense() << std::endl;
+			}
+		}
+		
+		//! PRINT THE THINGS
+	}
+	void search_value(int intpart, int fracpart) {
+		for (size_t i = 0; i < list.size(); i++)
+		{
+			if (list[i].higher_than(intpart))
+		}
+		
+	}
+	void add_entry(Expense entry, std::string filename) {
+		//* add the expense to the list
+		list.push_back(entry);
+		//* and write it to the file
+		std::ofstream contentFile;
+		contentFile.open(filename);
+		contentFile << entry.print_expense() << "  " << entry.original_description;
+		contentFile.close();
+	}
+};
+
+
+int main(int argc, char *argv[])
 {
+	Container container;
+	//* Error checking and loading from file
+	switch (argc)
+	{
+	case 1:
+		std::cout << "Please use the file name of the Expenses file as an argument.\n";
+		break;
+	case 2:
+		container.load(argv[1]);
+		break;
+	default:
+		std::cout << "Please enter a single file name as an argument.\n";
+		break;
+	}
 
-	// test_date();
-	// test_employee();
-	test_car();
+	Expense test(420,69,"A BIG REE");
+	test.print(); std::cout << std::endl;
+	Expense test1(4220,69,"A BIG REE");
+	test1.print(); std::cout << std::endl;
+	Expense test2(42220,69,"A BIG REE");
+	test2.print(); std::cout << std::endl;
+	Expense test3(0,0,"A BIG REE");
+	test3.print(); std::cout << std::endl;
 	return 0;
 }
